94d531ee5ec3fae8c3346e259df96afc13118ae4
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-10 13:32:59 -0500

    Converted the 'watch' functionality for listening to changes in our state, to computing the same values in a more efficient way.

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -2,7 +2,7 @@
   import Question from '../components/Question.vue' 
   import QuizHeader from '../components/QuizHeader.vue'
   import { useRoute } from 'vue-router'
-  import { ref, watch } from 'vue'
+  import { ref, watch, computed } from 'vue'
   import quizes from '../assets/data/quizzes.json'
   
   const route = useRoute()
@@ -13,11 +13,24 @@
 
   const currentQuestionIndex = ref(0)
 
-  const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+  // Watch allows us to listen to specific values in our state for changes. 
+  
+  // const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
 
-  watch(() => currentQuestionIndex.value, () => {
-    questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
-  })
+  // watch(() => currentQuestionIndex.value, () => {
+  //   questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+  // })
+
+  // The variation below is a more efficient use of state via computing the values. The reason 
+  // being that we don't need to use ref and watch, we just define the values to compute. 
+
+  // const questionStatus = computed(() => {
+  //   return `${currentQuestionIndex.value}/${quiz.questions.length}`
+  // })
+
+  // Even more efficient is writing the above code in one line, turning 5 lines of code into 1.
+  const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
+
 </script>
 
 <template>
