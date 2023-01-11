db4cf0aba7eb4706c4e26b890691ba2afd571e00
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-11 13:31:45 -0500

    Completed the Quiz app by introducing event emissions to pass data from the results component(child) to the QuizView view(parent), used conditional rendering to display number of questions answered correctly and the user has finished the quiz. Lastly, I added a Router Link to direct the user back to the home page to take another quiz, once they arrive at their quiz results

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Question from '../components/Question.vue' 
   import QuizHeader from '../components/QuizHeader.vue'
+  import Result from '../components/Result.vue'
   import { useRoute } from 'vue-router'
   import { ref, watch, computed } from 'vue'
   import quizes from '../assets/data/quizzes.json'
@@ -12,6 +13,10 @@
   const quiz = quizes.find(q => q.id === quizId)
 
   const currentQuestionIndex = ref(0)
+
+  const numberOfCorrectAnswers = ref(0)
+
+  const showResults = ref(false)
 
   // Watch allows us to listen to specific values in our state for changes. 
 
@@ -30,8 +35,18 @@
 
   // Even more efficient is writing the above code in one line, turning 5 lines of code into 1.
   const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`)
-
   const barPercentage = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length * 100}%`)
+
+  const onOptionSelected = (isCorrect) => {
+    if(isCorrect) {
+      numberOfCorrectAnswers.value++
+    }
+
+    if(quiz.questions.length - 1 === currentQuestionIndex.value) {
+      showResults.value = true
+    }
+    currentQuestionIndex.value++
+  }
 </script>
 
 <template>
@@ -41,8 +56,18 @@
       :barPercentage="barPercentage"
     />
     <div>
-      <Question :question="quiz.questions[currentQuestionIndex]"/>
+      <Question 
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Result 
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
+
     <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
